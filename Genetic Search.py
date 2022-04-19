eacddfd25a138ec0016075e0c1f436cfc71cb014
McKnightA <68346774+McKnightA@users.noreply.github.com>
2022-04-18 20:04:58 -0400

    Add files via upload
    with an option for evolving a path or evolving a state action policy

---
+++ Genetic Search.py
@@ -0,0 +1,118 @@
+import random
+import gym
+
+
+def spawnIndividual(length):
+    geneticCode = random.choices([0, 1, 2, 3, 4, 5], k=length)
+    return geneticCode
+
+
+class Population:
+
+    def __init__(self, popSize, pathLength):
+        if popSize > 0 and pathLength > 0:
+            self.fitnesses = []
+            self.individuals = []
+            for i in range(popSize):
+                self.individuals.append(spawnIndividual(pathLength))
+        else:
+            print("WRONG!! both inputs of population size and action sequcence length must be positive")
+
+    def checkPathFitness(self, env):
+        for indv in self.individuals:
+            personalFitness = 0
+            env.reset()
+            for action in indv:
+                observation, reward, done, ignore = env.step(action)
+                personalFitness += reward
+                if done:
+                    personalFitness += 90
+                    break
+                elif list(env.decode(observation))[2] == 4:
+                    #pass
+                    personalFitness += 5
+            self.fitnesses.append(personalFitness)
+
+    def checkPolicyFitness(self, env):
+        if len(self.individuals[0]) >= 500:
+            for indv in self.individuals:
+                personalFitness = 0
+                observation = env.reset()
+
+                for i in range(1000):
+                    observation, reward, done, ignore = env.step(indv[observation])
+                    personalFitness += reward
+                    if done:
+                        break
+
+                self.fitnesses.append(personalFitness)
+        else:
+            print("wrong! an individual needs a length of at least 500, but anything past 500 is ignored")
+
+    def evolve(self, env, popType, surviveBySkill=0.25, surviveByChance=0.05, mutationRate=0.05):
+
+        # evaluate the population
+        self.fitnesses = []
+        if popType == 'path':
+            self.checkPathFitness(env)
+        elif popType == 'policy':
+            self.checkPolicyFitness(env)
+        else:
+            raise ValueError(popType, " type of population does not exist")
+        scores = zip(self.fitnesses, self.individuals)
+        sortedIndvs = [x[1] for x in sorted(scores, reverse=True)]
+
+        # trim the heard
+        skilled = sortedIndvs[:int(len(sortedIndvs) * surviveBySkill)]
+        lucky = random.sample(sortedIndvs[int(len(sortedIndvs) * surviveBySkill):],
+                              k=int(len(sortedIndvs) * surviveByChance))
+        parents = skilled + lucky
+
+        # mutate the survivors
+        mutated = random.sample(range(len(parents)), k=int(len(sortedIndvs) * mutationRate))
+        for index in mutated:
+            parents[index][random.randint(0, len(parents[index]) - 1)] = random.randint(0, 5)
+
+        # breed em and weep
+        children = []
+        while len(children) < len(self.individuals) - len(parents):
+            par1 = parents[random.randint(0, len(parents) - 1)]
+            par2 = parents[random.randint(0, len(parents) - 1)]
+            if par1 != par2:
+                split = int(len(par1) / 2)
+                children.append(par1[:split] + par2[split:])
+        self.individuals = parents + children
+
+
+if __name__ == "__main__":
+    # Evolve a path
+    pop = Population(200, 100)
+    environ = gym.make('Taxi-v3').env
+
+    pop.checkPathFitness(environ)
+    fit = pop.fitnesses
+    aveFit = [sum(fit) / len(fit)]
+    i = 0
+    while aveFit[-1] < 0 and i < 100:
+        pop.evolve(environ, 'path')
+        fit = pop.fitnesses
+        aveFit.append(sum(fit) / len(fit))
+        i += 1
+
+    print("path fitness", aveFit)
+
+    # Evolve a policy
+    pop = Population(200, 500)
+    environ = gym.make('Taxi-v3').env
+
+    pop.checkPolicyFitness(environ)
+    fit = pop.fitnesses
+    aveFit = [sum(fit) / len(fit)]
+    i = 0
+    while aveFit[-1] < 0 and i < 200:
+        pop.evolve(environ, 'policy')
+        fit = pop.fitnesses
+        aveFit.append(sum(fit) / len(fit))
+        i += 1
+
+    print("policy fitness", aveFit)
