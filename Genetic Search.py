f9a483e6d317f2f7a9aff0064ad822fea6786670
McKnightA <68346774+McKnightA@users.noreply.github.com>
2022-04-27 12:11:07 -0400

    Add files via upload
    changed population evolution to loop for a set amount of generations and output the best individual

--- Genetic Search.py
+++ Genetic Search.py
@@ -29,8 +29,8 @@ class Population:
                     personalFitness += 90
                     break
                 elif list(env.decode(observation))[2] == 4:
-                    #pass
-                    personalFitness += 5
+                    # pass
+                    personalFitness += 1
             self.fitnesses.append(personalFitness)
 
     def checkPolicyFitness(self, env):
@@ -39,7 +39,7 @@ class Population:
                 personalFitness = 0
                 observation = env.reset()
 
-                for i in range(1000):
+                for i in range(500):
                     observation, reward, done, ignore = env.step(indv[observation])
                     personalFitness += reward
                     if done:
@@ -49,7 +49,7 @@ class Population:
         else:
             print("wrong! an individual needs a length of at least 500, but anything past 500 is ignored")
 
-    def evolve(self, env, popType, surviveBySkill=0.25, surviveByChance=0.05, mutationRate=0.05):
+    def evolve(self, env, popType, generations=200, surviveBySkill=0.25, surviveByChance=0.05, mutationRate=0.05):
 
         # evaluate the population
         self.fitnesses = []
@@ -59,29 +59,50 @@ class Population:
             self.checkPolicyFitness(env)
         else:
             raise ValueError(popType, " type of population does not exist")
-        scores = zip(self.fitnesses, self.individuals)
-        sortedIndvs = [x[1] for x in sorted(scores, reverse=True)]
 
-        # trim the heard
-        skilled = sortedIndvs[:int(len(sortedIndvs) * surviveBySkill)]
-        lucky = random.sample(sortedIndvs[int(len(sortedIndvs) * surviveBySkill):],
-                              k=int(len(sortedIndvs) * surviveByChance))
-        parents = skilled + lucky
+        aveFit = [sum(self.fitnesses) / len(self.fitnesses)]
+        fitest = ([], -9999999)
+        i = 0
+        while i < generations:
+            scores = zip(self.fitnesses, self.individuals)
+            scores = sorted(scores, reverse=True)
+            sortedIndvs = [x[1] for x in scores]
 
-        # mutate the survivors
-        mutated = random.sample(range(len(parents)), k=int(len(sortedIndvs) * mutationRate))
-        for index in mutated:
-            parents[index][random.randint(0, len(parents[index]) - 1)] = random.randint(0, 5)
+            if scores[0][0] > fitest[1]:
+                fitest = (scores[0][1], scores[0][0])
 
-        # breed em and weep
-        children = []
-        while len(children) < len(self.individuals) - len(parents):
-            par1 = parents[random.randint(0, len(parents) - 1)]
-            par2 = parents[random.randint(0, len(parents) - 1)]
-            if par1 != par2:
-                split = int(len(par1) / 2)
-                children.append(par1[:split] + par2[split:])
-        self.individuals = parents + children
+            # trim the heard
+            skilled = sortedIndvs[:int(len(sortedIndvs) * surviveBySkill)]
+            lucky = random.sample(sortedIndvs[int(len(sortedIndvs) * surviveBySkill):],
+                                  k=int(len(sortedIndvs) * surviveByChance))
+            parents = skilled + lucky
+
+            # breed em and weep
+            children = []
+            while len(children) < len(self.individuals) - len(parents):
+                par1 = parents[random.randint(0, len(parents) - 1)]
+                par2 = parents[random.randint(0, len(parents) - 1)]
+                if par1 != par2:
+                    split = int(len(par1) / 2)
+                    children.append(par1[:split] + par2[split:])
+            self.individuals = parents + children
+
+            # mutate the survivors
+            mutated = random.sample(range(len(self.individuals)), k=int(len(sortedIndvs) * mutationRate))
+            for index in mutated:
+                self.individuals[index][random.randint(0, len(self.individuals[index]) - 1)] = random.randint(0, 5)
+
+            self.fitnesses = []
+            if popType == 'path':
+                self.checkPathFitness(env)
+            elif popType == 'policy':
+                self.checkPolicyFitness(env)
+
+            aveFit.append(sum(self.fitnesses) / len(self.fitnesses))
+
+            i += 1
+
+        return fitest[0], aveFit
 
 
 if __name__ == "__main__":
@@ -89,30 +110,30 @@ if __name__ == "__main__":
     pop = Population(200, 100)
     environ = gym.make('Taxi-v3').env
 
-    pop.checkPathFitness(environ)
-    fit = pop.fitnesses
-    aveFit = [sum(fit) / len(fit)]
-    i = 0
-    while aveFit[-1] < 0 and i < 100:
-        pop.evolve(environ, 'path')
-        fit = pop.fitnesses
-        aveFit.append(sum(fit) / len(fit))
-        i += 1
+    best_path, path_fitness_curve = pop.evolve(environ, 'path', 100)
+    print("path evolution\n")
 
-    print("path fitness", aveFit)
+    environ.reset()
+    for action in best_path:
+        environ.render()
+        _, __, done, ___ = environ.step(action)
+        if done:
+            break
+    environ.close()
+    print("\n", path_fitness_curve)
 
     # Evolve a policy
-    pop = Population(200, 500)
+    pop = Population(300, 500)
     environ = gym.make('Taxi-v3').env
 
-    pop.checkPolicyFitness(environ)
-    fit = pop.fitnesses
-    aveFit = [sum(fit) / len(fit)]
-    i = 0
-    while aveFit[-1] < 0 and i < 200:
-        pop.evolve(environ, 'policy')
-        fit = pop.fitnesses
-        aveFit.append(sum(fit) / len(fit))
-        i += 1
+    best_policy, policy_fitness_curve = pop.evolve(environ, 'policy', 10000)
+    print("\npolicy evolution\n")
 
-    print("policy fitness", aveFit)
+    observation = environ.reset()
+    for i in range(500):
+        environ.render()
+        observation, _, done, __ = environ.step(best_policy[observation])
+        if done:
+            break
+    environ.close()
+    print("\n", best_policy, "\n", policy_fitness_curve)
